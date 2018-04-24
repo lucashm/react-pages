c44349da2ff68dd851bbfb2c63767f6fe030693a
Lucas Malta <lucas50xd@hotmail.com>
2018-04-24 15:33:52 -0300

    makes header responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
+import Menu from './menu/Menu';
 import './header.scss';
 
 class Header extends Component {
   render() {
     return (
       <div className="App-header">
+        <Menu></Menu>
         <div className="App-header-left">
+
           <img className="App-logo" src={logo} alt="reabilitacao-logo"></img>
           <h1>Reabilitação fisico-motora</h1>
         </div>
---
+++ src/components/header/menu/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './menu.scss';
+import IoMenu from 'react-icons/lib/io/android-menu';
+
+class Menu extends Component {
+  render() {
+    return (
+      <IoMenu className="Menu" />
+
+    );
+  }
+}
+
+export default Menu;
